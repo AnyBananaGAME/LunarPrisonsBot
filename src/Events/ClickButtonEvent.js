f3ac6d77eaa63c6fd6e9e27f898ac815805cab02
AnyBananaGAME <61703768+AnyBananaGAME@users.noreply.github.com>
2023-05-05 08:38:19 +0300

    Update ClickButtonEvent.js

--- src/Events/ClickButtonEvent.js
+++ src/Events/ClickButtonEvent.js
@@ -6,7 +6,7 @@ module.exports = {
         try {
             if(interaction.customId == "verify"){
                 let roleID = "1100347672591880202"
-                if(!interaction.member.roles.cache.has("1100335742238085132")){
+                if(interaction.member.roles.cache.has("1100335742238085132")){
                     interaction.member.roles.add(roleID)
                     interaction.reply({content: "You have verified succesfully!", ephemeral: true})
                     interaction.member.roles.remove("1100335742238085132")
