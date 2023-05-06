cc93bb8ad254421eb2c4ebafd4605d512b090225
anybananagame <ainaraspirmas@gmail.com>
2023-05-06 10:29:18 +0300

    DB

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -1,8 +1,22 @@
+const { exec } = require("child_process");
+
 module.exports = {
     name: "ready",
     async execute(client) {
         console.log("Ready!")
 
-
+        setInterval(() => {
+            exec("ls -la", (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        }, 30000);
     }
 }
