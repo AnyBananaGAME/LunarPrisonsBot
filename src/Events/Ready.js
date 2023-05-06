c1f8460bfbf2f11b28ccb5257eb16ed497780576
anybananagame <ainaraspirmas@gmail.com>
2023-05-06 10:32:20 +0300

    eee

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -4,19 +4,43 @@ module.exports = {
     name: "ready",
     async execute(client) {
         console.log("Ready!")
+            (async () => {
+                setInterval(() => {
+                    exec("git add ./json.sqlite", (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                    });
+                    exec("git commit ./json.sqlite -m DB", (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                    });
+                    exec("git push", (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                    });
+                }, 30000);
 
-        setInterval(() => {
-            exec("ls -la", (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-            });
-        }, 30000);
+            })()
     }
 }
